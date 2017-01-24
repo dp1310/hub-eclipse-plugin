f7650710a6e72de2bd06f550ace81ebc8c9b88a5
rotte <rotte@10.2.4.59>
2017-01-24 15:33:49 -0500

    Refactor- removed unused code

--- bundles/plugin/src/com/blackducksoftware/integration/eclipseplugin/common/services/HubRestConnectionService.java
+++ bundles/plugin/src/com/blackducksoftware/integration/eclipseplugin/common/services/HubRestConnectionService.java
@@ -1,12 +1,9 @@
 package com.blackducksoftware.integration.eclipseplugin.common.services;
 
-import java.net.URISyntaxException;
-
 import com.blackducksoftware.integration.exception.EncryptionException;
 import com.blackducksoftware.integration.hub.exception.HubIntegrationException;
 import com.blackducksoftware.integration.hub.global.HubServerConfig;
 import com.blackducksoftware.integration.hub.rest.CredentialsRestConnection;
-
 
 /*
  * Wrapper class for testing purposes
